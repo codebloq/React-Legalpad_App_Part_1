aee8c52a8b86f30a5b5fb34d08a800c932688e78
The Advocate <134937174+lawyerWho@users.noreply.github.com>
2023-05-30 15:26:24 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer>
+      {" "}
+      <p>Copyright © 2023-{currentYear}, lawyerWho.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Title goes here</h1>
+      <p>Content goes here</p>
+    </div>
+  );
+}
+
+export default Note;
